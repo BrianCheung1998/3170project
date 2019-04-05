89015b638bd7fcd60907d6a6ab5cff22d202bffb
SAMY2510 <1155093276link.cuhk.edu.hk>
2019-04-05 19:10:32 +0800

    First Function of employer done

--- run/src/Employer.java
+++ run/src/Employer.java
@@ -6,75 +6,64 @@ import java.sql.*;
 import java.text.*;
 
 public class Employer {
-    static String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db26";
-    static String dbUsername = "Group26";
-    static String dbPassword = "group26";
-    public static Connection con = null;
-
-    public static Statement stmt = null;
-    public static ResultSet resultSet = null;
     String sql = null;
     ConnectToMySQL DataBase = new ConnectToMySQL();
 
     public int post_position_recruitment(String Employer_ID, String Position_Title, int Salary, int Experience){
         int Temp_Position_ID = 0;
         int Suitable_Count = 0;
-        try{
-            Connection con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
-        }
-        catch(Exception Connection_e){
-            System.out.println("Connect Fail");
-        }
         //setup the sql query
         sql =
-                "SELECT E.Skills, E.Expected_Salary, E.Experience"+
-                "FROM Employment_History EH, Employee E"+
+                "SELECT E.Skills, E.Expected_Salary, E.Experience "+
+                "FROM Employment_History EH, Employee E "+
                 "WHERE (EH.Employee_ID = E.Employee_ID and EH.End IS NOT NULL)"+ // not working for any company currently
                 " or E.Employee_ID NOT IN("+
-                    "SELECT EH2.Employee_ID"+
+                    "SELECT EH2.Employee_ID "+
                     "FROM Employment_History EH2)"+
-                ")";
+                // employee's expected salary is no larger than the upper-bound of the salary
+                // employee's experience no less than input experience
+                " and E.Expected_Salary <= " + Salary +
+                " and E.Experience >= " + String.valueOf(Experience);
 
         String Set_SQL =
                 "INSERT INTO Position_Table (Position_ID, Position_Title, Salary, Experience, Employer_ID, Status)" +
-                "VALUE (" + String.format("%010d", Temp_Position_ID) + Position_Title + String.valueOf(Salary) +
-                String.valueOf(Experience) + Employer_ID + "TRUE"+")";
+                "VALUE (" + String.format("%010d", Temp_Position_ID) + ", " + Position_Title +  ", " + String.valueOf(Salary) +
+                ", " + String.valueOf(Experience) +  ", " + Employer_ID +  ", " + "TRUE"+")";
 
         try{ //check if there is any suitable employee
-            Statement stmt = con.createStatement();
-            ResultSet resultSet = stmt.executeQuery(sql);
+            DataBase.sta = DataBase.con.createStatement();
+            DataBase.rSet = DataBase.sta.executeQuery(sql);
+
             //DataBase.rSet = DataBase.sta.executeQuery(sql);
-            while(resultSet.next()){
-                String skills = resultSet.getString("skills");
-                int expected_salary = resultSet.getInt("expected_salary");
-                int experience = resultSet.getInt("experience");
+            while(DataBase.rSet.next()){
+                String skills = DataBase.rSet.getString("skills");
+                int expected_salary = DataBase.rSet.getInt("expected_salary");
+                int experience = DataBase.rSet.getInt("experience");
                 String[] Skill_Set = skills.split(";");
                 int Skill_Set_Size = Skill_Set.length;
                 Boolean Have_Skill = Boolean.FALSE;
-                // employee's expected salary is no larger than the upper-bound of the salary
-                // employee's experience no less than input experience
-                if(experience >= Experience && expected_salary <= Salary){
-                    for(int i = 0; i < Skill_Set_Size; i++){
-                        if(Skill_Set[i].equals(Position_Title)){
-                            Have_Skill = Boolean.TRUE;
-                            break;
-                        }
+                for(int i = 0; i < Skill_Set_Size; i++){
+                    if(Skill_Set[i].equals(Position_Title)){
+                        Have_Skill = Boolean.TRUE;
+                        break;
                     }
                 }
                 if(Have_Skill){ // skills should contain the position title
                     Suitable_Count++;
                 }
             }
-            stmt.close();
+            DataBase.sta.close();
         }
         catch (Exception e){
             System.err.println("Error occur when getting data for Position Recruitment");
             System.err.println(e.getMessage());
         }
         // if there is no record meet the requirement, return error
-        if(Suitable_Count > 0){
+        if(Suitable_Count == 0){
             try{
+                DataBase.sta = DataBase.con.createStatement();
                 DataBase.sta.executeUpdate(Set_SQL);
+                DataBase.sta.close();
             }
             catch(Exception e2){
                 System.err.println("Error occur when positing Position Recruitment");
@@ -110,7 +99,10 @@ public class Employer {
         /* List of person history */
 
         // check if the position is marked by the employee and posted by the employer
-        
+        String Get_Position =
+                "SELECT M.Position_ID FROM marked M, Position_Table P " +
+                "WHERE M.Position_ID = P.Position_ID and M.Employee_ID="
+                + Employee_ID + " and P.Employer_ID=" + Employer_ID;
         // if suitable, create employment history record
 
         // if suitable, changes the status of the "marked" record to be invalid
